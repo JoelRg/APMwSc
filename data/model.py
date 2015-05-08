90bc7e016390a8d52781c6ba5f9b04726b05036a
Joel <rivasjoel004@gmail.com>
2015-05-08 17:37:16 -0430

    model

--- data/model.py
+++ data/model.py
@@ -1,8 +1,69 @@
 
+# Librerias a utilizar.
 
-import os 
-basedir = os.path.abspath(os.path.dirname(__file__)) 
- 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 
-'apl.db') 
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+import configuraciondb   
+
+
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+
+db = declarative_base()
+
+# Tablas de la base de datos a definir.
+
+# Tabla Usuario.
+
+class User(db):
+    __tablename__ = 'user'
+    fullname = Column(String(50), unique = True)
+    username = Column(String(16), primary_key = True)
+    password = Column(String(16), nullable = False)
+    email = Column(String(30), unique = True)
+    iddpt = Column(Integer, ForeignKey('dpt.iddpt'), nullable = False)
+    idrole = Column(Integer, ForeignKey('role.idrole'), unique = True)
+    
+    def __init__(self,fullname, username, password, email, iddpt, idrole):
+        self.fullname = fullname
+        self.username = username
+        self.password = password
+        self.email = email
+        self.iddpt = iddpt
+        self.idrole = idrole
+
+# Tabla Departamento.
+
+class Dpt(db):
+    __tablename__ = 'dpt'
+    iddpt = Column(Integer, primary_key = True)
+    namedpt = Column(String(50), unique = True)
+    users = relationship('User', backref = 'dpt', cascade="all, delete, delete-orphan")
+
+    def __init__(self, iddpt, namedpt):
+        self.iddpt = iddpt
+        self.namedpt = namedpt
+
+# Tabla Role.
+
+class Role(db):
+    __tablename__ = 'role'
+    idrole = Column(Integer, primary_key = True)
+    namerole = Column(String(50), unique = True)
+    users = relationship('User', backref = 'role', cascade="all, delete, delete-orphan")
+
+    def __init__(self, idrole, namerole):
+        self.idrole = idrole
+        self.namerole = namerole
+        
+#-------------------------------------------------------------------------------
+
+# Se crea el motor que almacenara los datos en el directorio local.
+engine = create_engine(URL(**configuraciondb.DATABASE))    
+
+#Se eliminnan las tablas previamente definidas
+db.metadata.drop_all(engine)
+
+# Se crean todas las tablas definidas en el motor antes construidos.
+db.metadata.create_all(engine)
